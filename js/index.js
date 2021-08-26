dba46577f62dc5389201468c2d28b9223ce836e1
Sergio Puente <sergio.leones@hotmail.com>
2021-08-26 14:00:36 +0200

    First Dom

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
+function displayInitial() {
+    document.getElementById("canvas").style.display = "initial"
+    document.getElementById("second-page").style.display = "initial"
+    document.getElementById("story-box").style.display = "initial"
+    document.getElementById("first-page").style.display = "none"
+}
+
+function displayNone() {
+    document.getElementById("story-box").style.display = "none"
+    document.getElementById("second-page").style.display = "none"
+}
+
+
 window.onload = () => game.init('canvas')
+
+
